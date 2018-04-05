f1c78ab2cb97b05ab8fcbb8963e7a0368bf9cc94
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 19:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts (facts) {
-  var i = 1
+  var i = 0
   var result = []
   while (i < facts.length) {
     result[i] = facts[i] + "!!!";
