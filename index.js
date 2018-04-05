f3cfeb23a14f9897db68cece9dea19b8575a7363
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 19:20:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesPlay([musicians], [instruments]) {
+function theBeatlesPlay(musicians, instruments) {
   var result = []
   for (var i = 0; i < 10; i++) {
    result[i] = musicians[i] + " plays " + instruments[i] + " guitar"
